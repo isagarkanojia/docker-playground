c8b4dd30b53dbfad235ef4b0d5e7ddb982115c51
Sagar Kanojia <isagarkanojia@gmail.com>
2022-10-15 13:11:11 +0530

    externalise mongo config to env level

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
 mongoose
-  .connect("mongodb://sagar:key@mongo:27017/?authSource=admin")
+  .connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+  )
   .then(() => console.log("successfully connected to DB"))
   .catch((e) => console.log(e));
 
