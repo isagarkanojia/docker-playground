0e347abbe813086276f22860540774123e7165b2
Sagar Kanojia <isagarkanojia@gmail.com>
2022-10-15 13:16:48 +0530

    resolve warnings

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ const {
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+//TODO some issue with this config, check later
+const mongoConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
 mongoose
-  .connect(
-    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-  )
+  .connect(mongoURL)
   .then(() => console.log("successfully connected to DB"))
   .catch((e) => console.log(e));
 
